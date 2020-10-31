52cf578da430b8c5e2cd86359b7881fac9dfa7f2
wota-n <najmeenajib99@gmail.com>
2020-10-31 17:05:32 +0800

    add register activity

---
+++ app/src/main/java/com/inti/bayviewhotel/RegisterUsers.java
@@ -0,0 +1,13 @@
+package com.inti.bayviewhotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_users);
+    }
+}
